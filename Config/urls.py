a80e1df0a8f054c373f43e0e4f8a5662f538f6f8
Raisul Islam <ce.ripalash@gmail.com>
2024-12-24 21:38:50 +0200

    comment model added

--- Config/urls.py
+++ Config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
+    path('blog/', include('blogs.urls')),
     path('', include('website.urls')),
 ]
--- blogs/models.py
+++ blogs/models.py
@@ -13,13 +13,22 @@ class Post(BaseModel):
         title=models.CharField(max_length=100),
         content=models.TextField(null=True, blank=True)
     )
-
+    image = models.ImageField(upload_to='blog/images/', default='default.png')
     slug = models.SlugField(unique=True, db_index=True, editable=False)
     date_time = models.DateTimeField(null=True, blank=True)
     post_type = models.SmallIntegerField(choices=PostType.choices)
+    is_published = models.BooleanField(default=False)
 
     def save(self, *args, **kwargs):
         title = self.safe_translation_getter("title")
         if title and not self.slug:
             self.slug = slug_utils.generate_unique_slug(self, title)
         super().save(*args, **kwargs)
+
+
+class Comment(BaseModel):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    desc = models.CharField(max_length=600)
