5da6779beac8c4c4c04c843c7c3dd18f98e09250
Raisul Islam <p4l4s6@users.noreply.github.com>
2025-02-14 14:54:53 +0600

    Fixed Hashing problem for Password

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.hashers import make_password
 from django.apps import apps
 from parler.admin import TranslatableAdmin
 from parler.models import TranslatableModel
@@ -7,6 +9,14 @@ from blogs.admin import CommentAdmin
 from blogs.models import Post, Comment
 from prayertimes.admin import PrayerTimeAdmin
 from prayertimes.models import PrayerTime
+from coreapp.models import User
+
+# Custom admin for User to ensure password hashing
+class CustomUserAdmin(UserAdmin):
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data.get("password") and not obj.password.startswith("pbkdf2_"):
+            obj.password = make_password(form.cleaned_data["password"])
+        super().save_model(request, obj, form, change)
 
 models = apps.get_models()
 
@@ -18,8 +28,7 @@ for model in models:
             admin.site.register(PrayerTime, PrayerTimeAdmin)
         elif isinstance(model, type) and issubclass(model, Comment):
             admin.site.register(Comment, CommentAdmin)
+        elif isinstance(model, type) and issubclass(model, User):
+            admin.site.register(User, CustomUserAdmin)
         else:
             admin.site.register(model)
-
-
-
