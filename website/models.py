35fe21ecb9fb2837ecc61247cd78b51b9c62b476
FuzelahamedShaik <fuzelahamed1999@gmail.com>
2025-05-21 16:54:28 +0300

    creating models views and urls

--- website/models.py
+++ website/models.py
@@ -101,4 +101,4 @@ class Testimonial(BaseModel):
     role = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='website/pricing/')
+    image = models.ImageField(upload_to='website/home/')
